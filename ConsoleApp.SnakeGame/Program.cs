cd7efc0717a31b6c98c0bb163effbb1a4e659a9e
VelderGoose <vlniiv@mail.ru>
2024-01-03 00:18:14 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp.SnakeGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace ConsoleApp.SnakeGame
+{
+    internal class Program
+    {
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+        static void Main(string[] args)
+        {
+            SetWindowSize(MapWidth, MapHeight);
+            SetBufferSize(MapWidth, MapHeight);
+            CursorVisible = false;
+
+            ReadKey();
+        }
+    }
+    }
+}
